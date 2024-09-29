d48bb9109267ecede40242755a5f42666315e0a8
Messmanno <mesmercoulibaly2002@gmail.com>
2024-09-29 11:00:12 +0000

    first commit

---
+++ src/main/java/com/mycompany/dvdstore/demowithwebsocker/DemowithwebsockerApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.dvdstore.demowithwebsocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemowithwebsockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemowithwebsockerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mycompany/dvdstore/demowithwebsocker/model/MessageType.java
@@ -0,0 +1,6 @@
+package com.mycompany.dvdstore.demowithwebsocker.model;
+
+public enum MessageType {
+
+    CHAT,JOIN,LEAVE
+}
